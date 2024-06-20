b966a93ad9d5f6d46460b87e3def3c38b077c1b8
Mark Malstrom <mark@malstrom.me>
2024-06-20 18:23:03 -0500

    Add 'Group' prototype API comments

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,17 @@ import 'std/dotenv/load.ts';
 
 new Server()
     .register([
+        // new Group('/foo', [
+        //     FooHandler,
+        //     BarHandler,
+        //     // /foo/baz
+        //     new Group('/baz', [
+        //         BazHandler,
+        //         BaxHandler,
+        //     ])
+        //         .environment(new BaxClient()),
+        // ])
+        //     .environment(new BarClient()),
         SearchHandler,
         AirDateHandler,
         SubscriptionHandler,
