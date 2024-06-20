e75e66503c4df80f861514bc61fac67b34a8d678
Tanner Bennett <tannerbennett@me.com>
2024-06-19 23:19:00 -0500

    Refactor Trakt API client; add airDates()

--- src/trakt/client.ts
+++ src/trakt/client.ts
@@ -1,29 +1,63 @@
 import { API } from '../resty.ts';
-import 'std/dotenv/load.ts';
-import { SearchResult, ShowSearchResult } from './models/search-result.ts';
+import {
+    MovieSearchResult,
+    SearchResult,
+    ShowSearchResult,
+} from './models/search-result.ts';
+
+export interface TraktAirDate {
+    airs: {
+        day: string;
+        time: string;
+        timezone: string;
+    };
+}
 
 export class Trakt extends API {
     protected readonly baseUrl = 'https://api.trakt.tv';
 
-    private readonly apiKey = Deno.env.get('TRAKT_CLIENT_ID')!;
-    // private readonly apiSecret = Deno.env.get('TRAKT_API_SECRET')!;
-
-    private async search<T extends SearchResult>(
-        query: string,
-        type: 'show' | 'movie',
-    ) {
-        return await this
-            .get('/search')
-            .header('trakt-api-key', this.apiKey)
-            .searchParams({ query, type })
-            .fetchJson<T[]>();
+    constructor(private apiKey: string) {
+        super();
     }
 
-    async searchShows(query: string) {
-        return await this.search<ShowSearchResult>(query, 'show');
+    async search(
+        options: { query: string; type: 'movie' },
+    ): Promise<MovieSearchResult[]>;
+    async search(
+        options: { query: string; type: 'show' },
+    ): Promise<ShowSearchResult[]>;
+    async search(
+        options: { id: string; type: 'movie' },
+    ): Promise<MovieSearchResult[]>;
+    async search(
+        options: { id: string; type: 'show' },
+    ): Promise<ShowSearchResult[]>;
+    async search<Result extends SearchResult>(
+        options: { query: string; id?: undefined; type: 'movie' | 'show' } | {
+            query?: undefined;
+            id: string;
+            type: 'movie' | 'show';
+        },
+    ) {
+        return await this.get(
+            `search${options.id !== undefined ? `/tmdb/${options.id!}` : ''}`,
+        )
+            .header('trakt-api-key', this.apiKey)
+            .searchParams({ query: options.query, type: options.type })
+            .fetchJson<Result[]>();
+    }
+
+    async airDates({ showId }: { showId: string }) {
+        return await this.get(`shows/${showId}`)
+            .header('trakt-api-key', this.apiKey)
+            .searchParams({ extended: 'full' })
+            .fetchJson<TraktAirDate>()
+            .then(({ airs }) => airs);
     }
 }
 
-// const client = new Trakt();
-// console.log(`${client.apiKey}\n${client.apiSecret}`);
-// console.log((await client.searchShows('Breaking Bad'))[0]);
+// import 'std/dotenv/load.ts';
+// const apiKey = Deno.env.get('TRAKT_CLIENT_ID')!
+// console.log(apiKey)
+// const client = new Trakt(apiKey);
+// console.log((await client.search({ query: 'Breaking Bad', type: 'show' }))[0]);
