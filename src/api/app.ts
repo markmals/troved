a2789ebc6b5ebb9652a352c26cafb3fe6f499ddd
Mark Malstrom <mark@malstrom.me>
2024-08-08 21:22:10 -0500

    Organize imports

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,7 +1,9 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
+
 import airDates from './routes/air-dates.ts';
 import search from './routes/search.ts';
 import subscribe from './routes/subscribe.ts';
+
 import { tmdb, trakt } from '../services/mod.ts';
 
 const app = new OpenAPIHono();
