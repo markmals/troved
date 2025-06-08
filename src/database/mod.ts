bea60727ac0ff7dfeb74892c43c46f1f2b409f78
Mark Malstrom <mark@malstrom.me>
2025-06-08 15:38:29 -0500

    Migrate to Drizzle

--- src/database/mod.ts
+++ src/database/mod.ts
@@ -1,7 +1,11 @@
-// import "jsr:@std/dotenv/load";
+import "jsr:@std/dotenv/load";
+import { DatabaseSync } from "node:sqlite";
+import { drizzle } from "@mizchi/drizzle-orm/dist/node-sqlite/index.js";
 import { assert } from "@std/assert";
+import { GuestBook } from "./schema.ts";
 
 const DATABASE_URL = Deno.env.get("DATABASE_URL");
 assert(DATABASE_URL, "Must define DATABASE_URL in .env file");
 
-export const kv = await Deno.openKv(DATABASE_URL);
+const client = new DatabaseSync(DATABASE_URL);
+export const db = drizzle(client, { schema: { guestBook: GuestBook } });
--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,37 +1,7 @@
-import { kv } from "./mod.ts";
+import { integer, sqliteTable, text } from "@mizchi/drizzle-orm/dist/sqlite-core/index.js";
 
-export interface GuestBook {
-    id: number;
-    name: string;
-    email: string;
-}
-
-export const GUEST_BOOK = "guest_book";
-
-const ID_COUNTER = "id_counter";
-export async function getId() {
-    let current = await kv.get<number>([ID_COUNTER]);
-
-    while (true) {
-        // If it doesn’t exist, seed it at 0
-        if (!current.value) {
-            current.value = 0;
-        }
-
-        // Prepare the updated value
-        const newValue = current.value + 1;
-
-        // Attempt an atomic commit: "check" ensures the counter
-        // is unchanged, then "set" to newValue
-        const result = await kv.atomic().check(current).set([ID_COUNTER], newValue).commit();
-
-        // If commit succeeds, return the new ID
-        if (result.ok) {
-            return newValue;
-        }
-
-        // Otherwise, someone else updated the counter first;
-        // read and try again
-        current = await kv.get<number>([ID_COUNTER]);
-    }
-}
+export const GuestBook = sqliteTable("guest_book", {
+    id: integer().primaryKey(),
+    name: text({ length: 255 }).notNull(),
+    email: text({ length: 255 }).notNull().unique(),
+});
