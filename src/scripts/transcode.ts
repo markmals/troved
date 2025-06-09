9b20a323f75fcb12f62cffb32869b41007a8035a
Mark Malstrom <mark@malstrom.me>
2025-06-09 01:34:38 -0500

    Fix type errors

--- src/scripts/transcode.ts
+++ src/scripts/transcode.ts
@@ -2,20 +2,21 @@ import { transcoder } from "$api/services/mod.ts";
 import { Command } from "@cliffy/command";
 import { resolve } from "@std/path";
 
-const { watch, outputMovies, outputTv, quality, bitrate, forceHevc } = await new Command()
-    .name("transcode")
-    .description("Transcode video files")
-    .option("-w, --watch <dir:string>", "Folder to watch", { required: true })
-    .option("-m, --output-movies <dir:string>", "Folder to output converted movies", {
-        required: true,
-    })
-    .option("-t, --output-tv <dir:string>", "Folder to output converted TV shows", {
-        required: true,
-    })
-    .option("-q, --quality <number:number>", "Quality setting")
-    .option("-b, --bitrate <number:string>", "Audio bitrate")
-    .option("-h, --force-hevc", "Force HEVC encoding")
-    .parse(Deno.args);
+const { options: { watch, outputMovies, outputTv, quality, bitrate, forceHevc } } =
+    await new Command()
+        .name("transcode")
+        .description("Transcode video files")
+        .option("-w, --watch <dir:string>", "Folder to watch", { required: true })
+        .option("-m, --output-movies <dir:string>", "Folder to output converted movies", {
+            required: true,
+        })
+        .option("-t, --output-tv <dir:string>", "Folder to output converted TV shows", {
+            required: true,
+        })
+        .option("-q, --quality <number:number>", "Quality setting")
+        .option("-b, --bitrate <number:string>", "Audio bitrate")
+        .option("-h, --force-hevc", "Force HEVC encoding")
+        .parse(Deno.args);
 
 // Expand relative paths to absolute paths
 const resolvedWatch = resolve(watch);
